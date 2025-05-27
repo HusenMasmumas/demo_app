8ccc5eeece2f9facb4151d1d41fd59f5c67565e3
husen <husen@zicure.com>
2025-05-27 17:02:42 +0700

    feat: 🎸 button

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -1,5 +1,6 @@
 import { useTranslations } from "next-intl";
 import { Link } from "@/i18n/navigation";
+import { Button } from "@/components/ui/button";
 
 export default function HomePage() {
   const t = useTranslations("HomePage");
@@ -7,6 +8,11 @@ export default function HomePage() {
     <div>
       <h1>{t("title")}</h1>
       <Link href="/about">{t("about")}</Link>
+      <div>
+        <Button className="bg-chart-5" variant="destructive">
+          Test
+        </Button>
+      </div>
     </div>
   );
 }
